18ffe64f1dafee4f00f6bc65ce01fc700f72758d
titosilva <jt.mat@hotmail.com>
2024-06-11 23:41:22 -0300

    first commit with gtest boilerplate

---
+++ test/test1.cpp
@@ -0,0 +1,5 @@
+#include <gtest/gtest.h>
+
+TEST(Test1, Test1) {
+  EXPECT_EQ(1, 1);
+}
---
+++ test/test2.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+#include "../src/simple.hpp"
+
+TEST(Test2, Test2) {
+  ASSERT_EQ(sum(1, 2), 3);
+}
